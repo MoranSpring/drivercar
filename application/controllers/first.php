77c5d3dc495d8b7e046040c1f7993bdc3ae9ceb1
KYLE <KYLE@KYLE-PC>
2015-03-08 00:01:50 +0800

    测试一

--- application/controllers/first.php
+++ application/controllers/first.php
@@ -27,8 +27,9 @@ class First extends CI_Controller {
 
     public function index() {
         $data['head']=$this->load->view('a_views/second');
-        $data['body']=$this->load->view('a_views/third');
         
+        $data['head']=$this->load->view('a_views/second');$data['head']=$this->load->view('a_views/second');
+        $data['body']=$this->load->view('a_views/third');
         $data['body']=$this->load->view('a_views/third');
         $data['body']=$this->load->view('a_views/third');
         $data['body']=$this->load->view('a_views/third');
