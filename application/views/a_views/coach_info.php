72975e908f11d23674f3a3d03fe2f3ea6e2d6cdf
Kyle <maliang@secway.net>
2015-03-09 17:31:14 +0800

    2015.3.8  提交5:00

---
+++ application/views/a_views/coach_info.php
@@ -0,0 +1,109 @@
+<div id="content" class="clearfix">
+    <div id="con-left">
+        <ul>
+            <li><span>驾培资讯</span></li>
+            <li><span>培训点信息</span></li>
+            <li><span>服务指南</span></li>
+            <li><span>教练信息</span></li>
+            <li><span>教学大纲</span></li>
+        </ul>
+    </div>
+    <div id="con-right">
+        <div id="con-nav">
+            <p><a href="void">首页</a> > <a href="void"> 驾培首页</a> > 教练信息</p>
+        </div>
+        <div id="con-con" class ="real_content">
+            <ul class="tab">
+                <li id="one1" class="selected-li" onclick="setTab('one', 1, 3)">自动定位</li>
+                <li id="one2" onclick="setTab('one', 2, 3)">地址选择</li>
+                <li id="one3" onclick="setTab('one', 3, 3)">搜索</li>
+            </ul>
+            <div class="pos_content">
+                <div id="content1">
+                    <p id="city_china_val">所在地区：
+                        <select class="province cxselect" data-value="浙江省" data-first-title="选择省" disabled="disabled"></select>
+                        <select class="city cxselect" data-value="杭州市" data-first-title="选择市" disabled="disabled"></select>
+                        <select class="area cxselect" data-value="西湖区" data-first-title="选择地区" disabled="disabled"></select>
+                    </p>
+                    <div id="map">
+                        <div id="allmap"></div>
+                    </div>
+                </div>
+                <div id="content2" style="display:none">
+                </div>
+                <div id="content3" style="display:none">
+                </div>
+            </div>
+
+            <div class ="real_content">
+                <ul class="tab">
+                    <li id="one1" class="selected-li">教练信息</li>
+                </ul>
+                <div class="sch_content clearfix">
+                    <ul class="coach_list">
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="coachimg am-img-bdrs" src="http://cn.bing.com/az/hprichv/LondonTrainStation_GettyRR_139321755_ZH-CN742316019.jpg">
+                                <div class="clearfix"><div class="coa_name">李建国</div><div class="coa_sch">南湖驾校</div></div>
+                                <div class="coa_price">100￥/h</div>
+                                <div class="">已有3218人评价</div>
+                                <div class="coa_comment">当前评分 <img class="comm_" src="<?= base_url() . 'application/images/star5.png' ?>" ><span>4.7</span>分</div>
+                            </a>
+                        </li>
+                        
+                      
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
---
+++ application/views/a_views/ser_info.php
@@ -0,0 +1,31 @@
+<div id="content" class="clearfix">
+    <div id="con-left">
+        <ul>
+            <li><span>驾培资讯</span></li>
+            <li><span>培训点信息</span></li>
+            <li><span>服务指南</span></li>
+            <li><span>教练信息</span></li>
+            <li><span>教学大纲</span></li>
+        </ul>
+    </div>
+    <div id="con-right">
+        <div id="con-nav">
+            <p><a href="void">首页</a> > <a href="void"> 驾培首页</a> > 服务指南</p>
+        </div>
+        <div id="con-con" class ="real_content">
+            <h2>套餐详解：</h2><br/>
+            <h3>（1）经典套餐：</h3><br/>
+            <p>    包含考试费、报名费、培训费等费用以及30个学时，除了补考费以外，不会有任何后续费用，没用完的学时可以返现。</p><br/>
+            <h3>（2）自由购买学时:</h3><br/>
+            <p>     会员通过平台报名参加考试但不购买套餐者，自由购买学时训练。另外会员也可不通过平台报名考试而仅仅买学时练车。</p><br/>
+            <h2>预约服务详情：</h2><br/>
+
+            <p>科目二服务：倒库、上坡起步与定点停车、S路与直角转弯、侧方位停车。</p><br/>
+            <p>科目三服务：</p><br/>
+            <p>实况陪练：</p><br/>
+            <p>安全文明驾驶从这里开始！</p><br/>
+        </div>
+    </div>
+</div>
+
+
