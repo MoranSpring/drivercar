0644c00efd2eebff70b7fe864f240bdbdf000a65
KYLE <KYLE@KYLE-PC>
2015-03-12 23:14:49 +0800

    2015.3.11 23:40  pc  第二次提交 登录功能添加

--- application/views/a_views/header_logined.php
+++ application/views/a_views/header_logined.php
@@ -7,7 +7,7 @@
             <li class="user_header">
                 <a href="<?= base_url() ?>index.php/first/login"><span>登录 <img src='http://www.xcwljy.cn/images/pixel.gif'style="float:right;vertical-align:middle;" class="user_header_img"/></span></a>
                     <ul class='menu-chir'id="chir">
-                        <li><a href="<?= base_url() . 'index.php/first/sch_info' ?>"><span>驾培资讯</span></a></li>
+                        <li><a href="<?= base_url() . 'index.php/first/sch_info' ?>"><span>驾培  资讯</span></a></li>
                         <li><a href="<?= base_url() . 'index.php/first/pos_info' ?>"><span>培训点信息</span></a></li>
                         <li><a href="<?= base_url() . 'index.php/first/ser_info' ?>"><span>服务指南</span></a></li>
                         <li><a href="<?= base_url() . 'index.php/first/coach_info' ?>"><span>教练信息</span></a></li>
