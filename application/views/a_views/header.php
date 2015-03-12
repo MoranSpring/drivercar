18a346a02f7e303ea62587295538d99bdc15a1a8
Kyle <maliang@secway.net>
2015-03-12 11:16:45 +0800

    2015.3.12  提交11:16

--- application/views/a_views/header.php
+++ application/views/a_views/header.php
@@ -2,7 +2,7 @@
             <nav class="clearfix">
                 <ul id="hea-ul">
                     <li>
-                        <span >首页</span>
+                        <a href="<?=base_url()?>index.php"><span >首页</span></a>
                     </li>
                     <li>
                         <a href="<?=base_url()?>index.php/first/login"><span>登录</span></a>
--- application/views/a_views/template.php
+++ application/views/a_views/template.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <title>驾途网-首页</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href='<?php echo base_url() . 'application/css/index.css' ?>'>
+        <link rel="stylesheet" type="text/css" href='<?=base_url()?>application/css/index.css'>
         <link rel="shortcut icon" href="<?php echo base_url() . 'application/images/iconfont-suo.png' ?>" type="image/x-icon">
         <script src="<?php echo base_url() . 'application/js/jquery-1.7.1.min.js' ?>" type="text/javascript"></script>
         <script src="<?php echo base_url() . 'application/js/jquery.cxselect.min.js' ?>" type="text/javascript"></script>
@@ -63,8 +63,6 @@ and open the template in the editor.
             }
         }
 
-
-            
         </script>
     </body>
 </html>
