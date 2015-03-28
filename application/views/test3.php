e342c5f61820133f51a8dc0e62c240608ca0f151
Kyle <maliang@secway.net>
2015-03-29 00:28:59 +0800

    2015.3.29 1:00 适配ie低版本，替换amazeui文件

--- application/views/test3.php
+++ application/views/test3.php
@@ -4,24 +4,24 @@
 
         <link rel="stylesheet" href="<?= base_url() ?>application/css/admin/amazeui.min.css"/>
         <link type="text/css" href="<?= base_url() ?>application/css/ml.css" rel="stylesheet">
-        <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
-        <script src="<?= base_url() ?>application/js/admin/amazeui.min.js"></script>
-        <script src="<?= base_url() ?>application/js/admin/amazeui.chosen.js"></script>
+        <script src="<?php echo base_url() . 'application/js/jquery-1.7.1.min.js' ?>" type="text/javascript"></script>
+        <script src="<?= base_url() ?>application/js/admin/amazeui.js"></script>
         ﻿<!--[if (gte IE 9)|!(IE)]><!-->
-        <script src="http://libs.baidu.com/jquery/2.1.1/jquery.min.js"></script>
-        <script src="assets/js/amazeui.js"></script>
+    <script src="<?php echo base_url() . 'application/js/admin/jquery.min.js' ?>" ></script>
+    <script src="<?= base_url() ?>application/js/admin/amazeui.min.js"></script>
         <!--<![endif]-->
 
         <!--[if lt IE 9]>
         <script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
         <script src="http://cdn.staticfile.org/modernizr/2.8.3/modernizr.js"></script>
-        <script src="assets/js/polyfill/rem.min.js"></script>
-        <script src="assets/js/polyfill/respond.min.js"></script>
-        <script src="assets/js/amazeui.legacy.min.js"></script>
+        <script src="<?= base_url() ?>application/js/admin/polyfill/rem.min.js"></script>
+        <script src="<?= base_url() ?>application/js/admin/polyfill/respond.min.js"></script>
+        <script src="<?= base_url() ?>application/js/admin/amazeui.legacy.min.js"></script>
         <![endif]-->
+        <script src="<?= base_url() ?>application/js/admin/app.js"></script>
     </head>  
     <body>   
-        <!--<p><input type="text" class="am-form-field" placeholder="日历组件" data-am-datepicker readonly/></p>-->
+        <p><input type="text" class="am-form-field" placeholder="日历组件" data-am-datepicker readonly/></p>
 
         <div>hello</div>
 
